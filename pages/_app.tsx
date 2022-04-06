a1bd91d2fd44be47382b81fd05780870f8315dd1
Junho Park <dwarfthema@gmail.com>
2022-04-07 00:33:01 +0900

    ~2.0 Nav & Seo Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,18 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: any) => {
   return (
-    <>
+    <Layout>
       <h1>Header</h1>
-
-      <NavBar />
       <Component {...pageProps} />
-
       <h1>Footer</h1>
-
       <style jsx global>{`
         .active {
           color: red;
         }
       `}</style>
-    </>
+    </Layout>
   );
 };
 
